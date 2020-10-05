3153ac0aa48c43e91605e958961f960c26a1944f
dwainejade <dwainejade@gmail.com>
2020-10-04 20:00:41 -0400

    addinf search feature

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -3,48 +3,82 @@ import axios from 'axios';
 import UserCard from './components/UserCard';
 import Followers from './components/Followers';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 class App extends React.Component {
   constructor() {
-    console.log("Constructor");
     super();
     this.state = {
       user: [],
-      followers: []
+      followers: [],
+      formInput: null,
+      searchResult: null
     }
+
   }
 
   componentDidMount() {
-    console.log('componentDidMount');
-    axios.get('https://api.github.com/users/dwainejade')
+    console.log("component did mount")
+    axios.get(`https://api.github.com/users/dwainejade`)
       .then((res) => {
         this.setState({ user: res.data })
-        console.log('user:', this.state)
       })
       .catch((err) => console.log('err', err))
 
-      axios.get('https://api.github.com/users/dwainejade/followers')
+    axios.get(`https://api.github.com/users/dwainejade/followers`)
       .then((res) => {
         this.setState({ followers: res.data })
-        console.log('followers:', this.state)
       })
-      .catch((err) => console.log('ferr', err))
+      .catch((err) => console.log('mount', err))
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.formInput !== this.state.formInput) {
+      axios.get(`https://api.github.com/users/${this.state.formInput}`)
+        .then((res) => {
+          this.setState({
+            ...this.state,
+            searchResult: res.data
+          });
+        }).catch(err => {
+          console.log(err)
+        })
+    }
 
-
-  componentDidUpdate(prevState, prevProps) {
-   
   }
 
+  mySubmitHandler = (e) => {
+    e.preDefault();
+    console.log("search submitted")
+    axios.get(`https://api.github.com/users/${this.state.formInput}`)
+      .then((res) => {
+        this.setState({ user: res.data })
+      })
+      .catch((err) => {
+        console.log("search", err)
+      })
+  }
+  myChangeHandler = (e) => {
+    this.setState({ formInput: e.target.value });
+  }
 
   render() {
-    console.log('Render');
-
     return (
       <div className="App">
         <h1>Github Profile</h1>
+        <form onSubmit={this.mySubmitHandler}>
+          <input
+            type='text'
+            className="p-1"
+            placeholder="Search user"
+            onChange={this.myChangeHandler}
+          />
+          <input
+            className="btn btn-primary btn-sm m-2"
+            type='submit'
+          />
+        </form>
         <UserCard user={this.state.user} />
         <h1>Followers:</h1>
         <Followers followers={this.state.followers} />
